de23d231c750de41b40dd584cd63de668e086873
Somnath Chakraborty <115790983+som-1998@users.noreply.github.com>
2024-05-25 17:03:45 +0530

    first commit

---
+++ som.js
@@ -0,0 +1,10 @@
+"use strict"
+let b;
+let a = [1, 2, 3, 6]
+let sum = 0
+a.forEach(function (e) {
+    sum += e
+})
+console.log(sum)
+
+// a.forEach((e,i,a) => console.log(e,i, a))
--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">TextUtils</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Home </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Link</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
 
