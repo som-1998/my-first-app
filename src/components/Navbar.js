2ac51532c8300351b2186d5b5344a4d3405fac6a
Somnath Chakraborty <115790983+som-1998@users.noreply.github.com>
2024-05-25 22:02:24 +0530

    000

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// function goToPreview() {
+
+// }
+
 export default function Navbar({ title = "Title", about = 'About' }) {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light mx-2">
                 <a className="navbar-brand" href="/">{title}</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -13,10 +17,10 @@ export default function Navbar({ title = "Title", about = 'About' }) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="/">Home </a>
+                            <a className="nav-link" href="/">Preview </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{ about}</a>
+                            <a className="nav-link" href="/">{about}</a>
                         </li>
                     </ul>
                 </div>
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -3,27 +3,52 @@ import PropTypes from 'prop-types';
 
 
 
-
-
 export default function TextBox({ heading = "Title" }) {
     const [text, setText] = useState('Enter the Text Here')
 
-    const handleOnClicke = () => {
-        console.log('UpperCase Is clicked')
+    const handleUpClicke = () => {
+        // console.log('UpperCase Is clicked')
         setText(text.toUpperCase())
+    }
+    const handleLoClicke = () => {
+        // console.log('UpperCase Is clicked')
+        setText(text.toLowerCase())
     }
 
     const handleOnChange = (event) => {
-        console.log('On Change')
+        // console.log('On Change')
         setText(event.target.value);
     }
+    const handleClear = (event) => {
+        // console.log('On Change')
+        setText('');
+    }
+
+    function noOfWords(val) {
+        if (val.length === 0) return 0;
+        else return val.trim().split(/\s+/).length;
+    }
+
+
     return (
-        <div className="form-group container">
-            <h3>{heading}</h3>
-            {/* <label for="exampleFormControlTextarea1">Example textarea</label> */}
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="5"></textarea>
-            <button className="btn btn-primary" onClick={handleOnClicke} >UpperCase</button>
-        </div>
+        <>
+            <div className="form-group container">
+                <h3>{heading}</h3>
+                {/* <label for="exampleFormControlTextarea1">Example textarea</label> */}
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="3"></textarea>
+                <button className="btn btn-primary  my-2 mx-1" onClick={handleUpClicke} >Upper Case</button>
+                <button className="btn btn-primary  my-2 mx-1" onClick={handleLoClicke} >Lower Case</button>
+                <button className="btn btn-primary  my-2 mx-1" onClick={handleClear} >Clear</button>
+            </div>
+            <div className="container my-2">
+                <h3>Text Analysis</h3>
+                <p>Your text has {noOfWords(text)} words and {text.length} characters. </p>
+            </div>
+            <div className="container">
+                <h3>Preview</h3>
+                {text}
+            </div>
+        </>
     );
 }
 
